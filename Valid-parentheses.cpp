f03c6ff7f4364e8e3e5724b068a4445c3ab4e82a
Shiwani <97833337+Shiwani-sahu@users.noreply.github.com>
2022-10-21 20:14:41 +0530

    Create Valid-parentheses.cpp

---
+++ Valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<char>st;
+        for(int i=0;i<s.length();i++){
+            char ch =s[i];
+        
+        if(ch=='(' || ch=='{' || ch=='['){
+            st.push(ch);
+        }
+        else {
+            if(!st.empty()){
+                char top =st.top();
+                if((ch == ')' && top=='(')||
+                (ch=='}' && top =='{') ||
+                (ch==']' && top =='[') ){
+                    st.pop();
+            }
+                else
+                {
+                    return false;
+                }
+            }
+                else
+                {
+                  return false;
+        }
+        }  
+    }
+     
+         
+        if (st.empty())
+            return true;
+        else 
+            return false;
+    }
+    
+        
+    
+};
